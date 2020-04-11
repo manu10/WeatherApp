789e37a1f20b70ca3a91c553fd7901a5ef53a110
Manuel <manugarcia010@gmail.com>
2020-04-11 10:30:01 -0300

    Add default project structure

---
+++ app/src/main/java/com/manugarcia010/weatherapp/ui/WeekForecastActivity.kt
@@ -0,0 +1,13 @@
+package com.manugarcia010.weatherapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manugarcia010.weatherapp.R
+
+class WeekForecastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_week_forecast)
+    }
+}
