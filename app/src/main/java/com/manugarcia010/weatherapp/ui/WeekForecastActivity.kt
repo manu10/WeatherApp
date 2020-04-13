0fc2a598bbcc276a7a8416649aaa8d4e3d42b77a
Manuel <manugarcia010@gmail.com>
2020-04-12 22:45:22 -0300

    Add WeekForecastViewModel, use it in the view with data binding

--- app/src/main/java/com/manugarcia010/weatherapp/ui/WeekForecastActivity.kt
+++ app/src/main/java/com/manugarcia010/weatherapp/ui/WeekForecastActivity.kt
@@ -1,10 +1,13 @@
 package com.manugarcia010.weatherapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.manugarcia010.weatherapp.R
+import com.manugarcia010.weatherapp.databinding.ActivityWeekForecastBinding
 
 class WeekForecastActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWeekForecastBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/manugarcia010/weatherapp/ui/WeekForecastViewModel.kt
@@ -0,0 +1,7 @@
+package com.manugarcia010.weatherapp.ui
+
+import androidx.lifecycle.ViewModel
+
+class WeekForecastViewModel : ViewModel() {
+
+}
