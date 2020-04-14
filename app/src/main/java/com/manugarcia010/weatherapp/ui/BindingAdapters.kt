e6bc1df218f7bbff51ad2e7e16253ce1cd4807c1
Manuel <manugarcia010@gmail.com>
2020-04-14 08:33:12 -0300

    Add databinding adapters

---
+++ app/src/main/java/com/manugarcia010/weatherapp/ui/BindingAdapters.kt
@@ -0,0 +1,43 @@
+package com.manugarcia010.weatherapp.ui
+
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import com.manugarcia010.weatherapp.ui.extensions.parentActivity
+
+
+/* DataBinding adapters for across the app */
+@BindingAdapter("adapter")
+fun setAdapter(view: RecyclerView, adapter: RecyclerView.Adapter<*>) {
+    view.adapter = adapter
+}
+
+@BindingAdapter("mutableVisibility")
+fun setMutableVisibility(view: View, visibility: MutableLiveData<Int>?) {
+    val parentActivity: AppCompatActivity? = view.parentActivity
+    if (parentActivity != null && visibility != null) {
+        visibility.observe(parentActivity, Observer { value ->
+            view.visibility = value ?: View.VISIBLE
+        })
+    }
+}
+
+@BindingAdapter("mutableText")
+fun setMutableText(view: TextView, text: MutableLiveData<String>?) {
+    val parentActivity: AppCompatActivity? = view.parentActivity
+    if (parentActivity != null && text != null) {
+        text.observe(parentActivity, Observer { value -> view.text = value ?: "" })
+    }
+}
+
+//@BindingAdapter("src") //todo:  add if necessary
+//fun setImageUrl(view: ImageView, url: String?) {
+//    Glide.with(view.context)
+//        .load(url)
+//        .placeholder(R.mipmap.placeholder)
+//        .into(view)
+//}
